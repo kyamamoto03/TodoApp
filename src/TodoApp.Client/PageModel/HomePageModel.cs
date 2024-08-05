3183f015839285e7b56d44827b7cb099c3f249a9
山本 浩司 <k.yamamoto03@iforcom.jp>
2024-08-05 15:08:40 +0900

    ClientでDomainModel利用

---
+++ src/TodoApp.Client/PageModel/HomePageModel.cs
@@ -0,0 +1,20 @@
+﻿namespace TodoApp.Client.PageModel;
+
+public interface IHomePageModel
+{
+    void CreateTodo();
+}
+
+public class HomePageModel() : IHomePageModel
+{
+
+    public void CreateTodo()
+    {
+        Todo.Domain.Todo todo = Todo.Domain.Todo.CreateNew("TITLE", "DESCRIPTION", new DateTime(2024, 8, 1), new DateTime(2024, 8, 2));
+        Todo.Domain.TodoItem todoItem = Todo.Domain.Todo.CreateNewTodoItem("ItemTitle", new DateTime(2024, 8, 1, 10, 0, 0), new DateTime(2024, 8, 2, 15, 0, 0));
+
+
+        todo.AddTodoItem(todoItem);
+    }
+
+}
---
+++ src/TodoApp.Client/Pages/Home.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+using TodoApp.Client.PageModel;
+
+namespace TodoApp.Client.Pages;
+
+public partial class Home
+{
+    [Inject]
+    public IHomePageModel _homePageModel { get; set; } = default!;
+}
