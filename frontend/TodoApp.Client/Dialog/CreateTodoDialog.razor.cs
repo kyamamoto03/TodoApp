f5c9dd819d7fcaedf7ecea45218d232c21b7cbdd
山本 浩司 <k.yamamoto03@iforcom.jp>
2024-08-30 14:44:51 +0900

    csファイルに移行

---
+++ frontend/TodoApp.Client/Dialog/CreateTodoDialog.razor.cs
@@ -0,0 +1,33 @@
+﻿using Blazored.FluentValidation;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace TodoApp.Client.Dialog;
+
+public partial class CreateTodoDialog
+{
+    [CascadingParameter]
+    private MudDialogInstance MudDialog { get; set; } = default!;
+
+    private CreateTodoDialogPageModel createTodoDialogPageModel = new();
+    private bool _isProcessing { get; set; } = false;
+
+    private FluentValidationValidator? _fluentValidationValidator;
+
+    private async Task CreateTodo()
+    {
+        try
+        {
+            _isProcessing = true;
+
+            if (await _fluentValidationValidator!.ValidateAsync())
+            {
+                MudDialog.Close(DialogResult.Ok(true));
+            }
+        }
+        finally
+        {
+            _isProcessing = false;
+        }
+    }
+}
