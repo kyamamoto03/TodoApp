24d9a6d075bb0f213b46a9df0094dde8f8999fab
山本 浩司 <k.yamamoto03@iforcom.jp>
2024-09-18 14:07:59 +0900

    パッケージ更新

--- test/TodoApp.Api.Test/ApiServiceFactory.cs
+++ test/TodoApp.Api.Test/ApiServiceFactory.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.Extensions.Logging;
-using Moq;
 using NSubstitute;
 using TodoApp.Api.Apis;
 
@@ -10,9 +9,6 @@ internal class ApiServiceFactory
     internal static ApiService Create()
     {
         var loggerMoq = Substitute.For<ILogger<ApiService>>();
-        loggerMoq.LogInformation(It.IsAny<string>());
-        loggerMoq.LogWarning(It.IsAny<string>());
-        loggerMoq.LogError(It.IsAny<string>());
 
         var _apiService = new ApiService(loggerMoq);
         return _apiService;
